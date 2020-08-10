3ce714fdef50041a131e59fcff1ceef2740bf01f
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-08-10 15:38:02 +0100

    quering documents part 2

--- mongodb.js
+++ mongodb.js
@@ -38,5 +38,42 @@ MongoClient.connect(
       .toArray((error, users) => {
         console.log(users);
       });
+
+    db.collection('users')
+      .find({ age: 27 })
+      .count((error, count) => {
+        console.log(count);
+      });
+
+    db.collection('tasks').findOne(
+      { _id: new ObjectID('5f2c1aff675ce054383e52ea') },
+      (err, tasks) => {
+        if (err) {
+          console.log('Unable to find task');
+        }
+
+        console.log(tasks);
+      }
+    );
+
+    db.collection('tasks')
+      .find({ completed: false })
+      .toArray((err, tasks) => {
+        if (err) {
+          console.log('Unable to find task');
+        }
+
+        console.log(tasks);
+      });
+
+    db.collection('tasks')
+      .find({ completed: false })
+      .count((err, tasks) => {
+        if (err) {
+          console.log('Unable to find task');
+        }
+
+        console.log('Number of uncompleted tasks: ' + tasks);
+      });
   }
 );
