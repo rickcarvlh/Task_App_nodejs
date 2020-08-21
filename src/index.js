82cce84258671c74a0e30765e3e084ec7d785319
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-08-21 15:16:12 +0100

    refactoring code async / await

--- src/index.js
+++ src/index.js
@@ -25,63 +25,71 @@ app.post('/users', async (req, res) => {
 
 // get users api
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
         res.send(users)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
 })
 
 // get users by id api
 
 // * needs to have a valid id -> 12 or 24 numbers
-app.get('/users/:id', (req, res) => {
+
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
-    User.findById(_id).then((user) => {
+
+    try {
+        const user = await User.findById(_id)
         if (!user) {
             return res.status(404).send()
         }
         res.send(user)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
 })
 
 // * task related routes
 
 // task api creating
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
-    task.save().then(() => {
+    try {
+        task.save()
         res.status(201).send(task)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
+    } catch (e) {
+        res.status(500).send()
+    }
 
 })
 
 // get  all task api
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((task) => {
-        res.status(200).send(task)
-    }).catch((e) => {
-        res.status(500).send(e)
-    })
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send()
+    }
 })
 
 // get tasks by id api
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
-    Task.findById(_id).then((task) => {
+    try {
+        const task = await Task.findById(_id)
         if (!task) {
             return res.status(404).send()
         }
-        res.status(200).send(task)
-    }).catch((e) => {
+        res.send(task)
+    } catch (e) {
         res.status(500).send()
-    })
+    }
+
 })
 
 app.listen(port, () => {
