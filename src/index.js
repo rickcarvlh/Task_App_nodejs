83b8b7fab989758eb6bdc8fe2b9ff6d5b0bc520a
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-08-24 15:22:52 +0100

    separete route files - task

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require('./db/mongoose')
 const User = require('./models/user');
 const Task = require('./models/task');
 const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 // calling express package
 const app = express();
@@ -10,89 +11,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(userRouter)
-
-
-// * task related endpoints
-
-// task api creating
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-
-    try {
-        task.save()
-        res.status(201).send(task)
-    } catch (e) {
-        res.status(500).send()
-    }
-
-})
-
-// * updating tasks endpoints
-// patch method
-app.patch('/tasks/:id', async (req, res) => {
-    // validation
-    const _id = req.params.id;
-    const updates = Object.keys(req.body)
-    const allowedUpadates = ['description', 'completed']
-    const isValidOperation = updates.every((update) => allowedUpadates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalida updates to task' })
-    }
-
-
-    try {
-        const task = await Task.findByIdAndUpdate(_id, req.body, { runValidators: true });
-        if (!task) {
-            return res.status(4040).send()
-        }
-        res.send(task)
-    }
-    catch (e) {
-        res.status(500).send(e)
-    }
-
-})
-
-// get  all task api
-app.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find({})
-        res.send(tasks)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-// get tasks by id api
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-        const task = await Task.findById(_id)
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    } catch (e) {
-        res.status(500).send()
-    }
-
-})
-
-// * deleting tasks by id
-app.delete('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-        const task = await Task.findByIdAndDelete(_id)
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
-
+app.use(taskRouter)
 
 
 app.listen(port, () => {
