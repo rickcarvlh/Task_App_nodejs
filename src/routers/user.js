ad72713b94392369dd79a41a7438e53d20f0effd
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-08-25 20:42:53 +0100

    Loggin in Users  - Login Enpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -16,6 +16,16 @@ router.post('/users', async (req, res) => {
     }
 })
 
+//  * LOGIN ENDPOINT
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
 // get users api
 
 router.get('/users', async (req, res) => {
