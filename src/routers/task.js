37e647bb2210785ecda1a777b4944cf624a46a90
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-09-16 11:57:31 +0100

    authenticating tasks endpoints

--- src/routers/task.js
+++ src/routers/task.js
@@ -63,10 +63,12 @@ router.get('/tasks', async (req, res) => {
 })
 
 // get tasks by id api
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
-        const task = await Task.findById(_id)
+        //const task = await Task.findById(_id)
+
+        const task = await Task.findOne({ _id, owner: req.user._id })
         if (!task) {
             return res.status(404).send()
         }
