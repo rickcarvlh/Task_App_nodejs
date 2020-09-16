c0c05064a17d64d1de5eab60e398e0f40d0d26c4
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-09-16 12:58:16 +0100

    final part of tasks endpoint

--- src/routers/task.js
+++ src/routers/task.js
@@ -77,10 +77,12 @@ router.get('/tasks/:id', auth, async (req, res) => {
 })
 
 // * deleting tasks by id
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
-        const task = await Task.findByIdAndDelete(_id)
+        const task = await Task.findOneAndDelete({
+            _id: req.params.id , owner: req.user._id
+        })
         if (!task) {
             return res.status(404).send()
         }
