85806ba094bed9f7740930429a7d2d1f12d0fcbb
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-09-17 16:39:12 +0100

    working with timestamps

--- src/index.js
+++ src/index.js
@@ -31,10 +31,10 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port);
 })
 
-const main =  async () => {
-    const user = await User.findById('5f5f4df09fe3c53d58a56e85')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks);
-}
+// // const main =  async () => {
+// //     const user = await User.findById('5f5f4df09fe3c53d58a56e85')
+// //     await user.populate('tasks').execPopulate()
+// //     console.log(user.tasks);
+// // }
 
-main()
+// main()
